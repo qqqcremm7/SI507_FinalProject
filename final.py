055d01f3bbede58d10bbd0545ea9aaf58ff759b2
qqqcremm7 <55447271+qqqcremm7@users.noreply.github.com>
2020-04-29 11:40:24 +0800

    Add files via upload

--- final.py
+++ final.py
@@ -7,13 +7,10 @@ import requests
 from flask import Flask, render_template
 from secret import client_id
 from secret import client_secret
-#from secret import API_KEY
-#import sys
-#from tabulate import tabulate
-#import plotly
-#import plotly.graph_objs as go
 
-#------------------------Scrap Data Part--------------------------------------------
+
+### Part 1: Scrap Data
+
 #def cache_uni_list():
 CACHE_FNAME_1 = 'cache_university_list.json'
 try:
@@ -23,8 +20,6 @@ try:
     cache_file.close()
 except:
     CACHE_DICTION_1 = {}
-        
-#    return CACHE_DICTION
 
 #def cache_uni_wiki():
 CACHE_FNAME_2 = 'cache_university_wiki.json'
@@ -35,24 +30,23 @@ try:
     cache_file.close()
 except:
     CACHE_DICTION_2 = {}
-#        return CACHE_DICTION
-    
+
+
+#use data in cache file to find universities, if it has cache, then use the cache data
+#else useAPI to get data in wiki and cache
 def find_university_list_using_cache(CACHE_DICTION_1):
     baseurl = 'https://en.wikipedia.org/w/api.php'
-#    baseurl = 'https://encyclopedia.thefreedictionary.com'
     paras = {}
     paras['action']='parse'
     paras['page']='List_of_state_and_territorial_universities_in_the_United_States'
     paras['format']='json'
-    
+
 #    CACHE_DICTION = cache_uni_list()
-    
+
     if 'universitylist' in CACHE_DICTION_1:
-#        print("Getting cached list data...")
         return CACHE_DICTION_1["universitylist"]
-    
+
     else:
-#        print("Making a request for new list data...")
         output = json.loads(requests.get(baseurl,paras).text)
         content = output['parse']['text']['*']
         CACHE_DICTION_1['universitylist'] = content
@@ -61,7 +55,7 @@ def find_university_list_using_cache(CACHE_DICTION_1):
         fw.write(dumped_json_cache)
         fw.close() # Close the open file
         return CACHE_DICTION_1['universitylist']
-    
+
 def scrap_university_list(page_text):
     page_soup = BeautifulSoup(page_text, 'html.parser')
     content = page_soup.find(class_='mw-parser-output')
@@ -87,16 +81,16 @@ def scrap_university_list(page_text):
             except:
                 pass
     return contentlist
-    
+
 def find_university_using_cache(university,CACHE_DICTION_2):
     baseurl = 'https://en.wikipedia.org/w/api.php'
     paras = {}
     paras['action']='parse'
     paras['page']=university
     paras['format']='json'
-    
+
 #    CACHE_DICTION = cache_uni_wiki()
-    
+
     if university in CACHE_DICTION_2:
 #        print("Getting cached wiki data...")
         return CACHE_DICTION_2[university]
@@ -110,7 +104,7 @@ def find_university_using_cache(university,CACHE_DICTION_2):
         fw.write(dumped_json_cache)
         fw.close() # Close the open file
         return CACHE_DICTION_2[university]
-    
+
 def scrap_university_info(page_text):
     page_soup = BeautifulSoup(page_text, 'html.parser')
     content = page_soup.find(class_='infobox vcard')
@@ -127,9 +121,9 @@ def scrap_university_info(page_text):
         infodic['geoinfo'] = content.find(class_='geo-dec').text
     except:
         pass
-    
+
     finaldic = {}
-    
+
     try:
         finaldic['University'] = content.find(class_='fn org').text
     except:
@@ -157,7 +151,8 @@ def scrap_university_info(page_text):
     return finaldic
 
 
-#------------------------Write into Database Part----------------------------------------
+### Part 2: Write into Database
+
 DBNAME = 'univeristy.db'
 rankingsfile = '2019-QS-World-University-Rankings2.csv'
 
@@ -216,7 +211,7 @@ def createdb(DBNAME):
     cur.execute(statement)
     conn.commit()
     conn.close()
-    
+
 def writeintorankings(DBNAME):
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
@@ -231,7 +226,7 @@ def writeintorankings(DBNAME):
             cur.execute(statement, insertion)
     conn.commit()
     conn.close()
-    
+
 def writeintowiki(DBNAME,CACHE_DICTION_1,CACHE_DICTION_2):
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
@@ -247,17 +242,15 @@ def writeintowiki(DBNAME,CACHE_DICTION_1,CACHE_DICTION_2):
             pass
     conn.commit()
     conn.close()
-    
-#------------------------It's the function that calls all above to scrap webs and write into databse------------
+
+### def the function calls all above to scrap webs and write into databse
 def createbdall(DBNAME,CACHE_DICTION_1,CACHE_DICTION_2):
     createdb(DBNAME)
     writeintorankings(DBNAME)
     writeintowiki(DBNAME,CACHE_DICTION_1,CACHE_DICTION_2)
-#createbdall()
-#print(scrap_university_info(find_university_using_cache('University_of_Minnesota')))
 
 
-#------------------------Output Part---------------------------------------------
+### Part 3: Output
 CACHE_FNAME3 = 'cache_idlist.json'
 
 try:
@@ -267,7 +260,7 @@ try:
     cache_file.close()
 except:
     CACHE_DICTION = {}
-    
+
 def find_idlist_using_cache():
     baseurl = 'https://api.foursquare.com/v2/venues/categories'
     paradic = {}
@@ -276,10 +269,8 @@ def find_idlist_using_cache():
     paradic['v'] = '20191103'
     unique_ident = paradic['v']
     if unique_ident in CACHE_DICTION:
-        #print("Getting cached data...")
         return CACHE_DICTION[unique_ident]
     else:
-        #print("Making a request for new data...")
         output = json.loads(requests.get(baseurl,params=paradic).text)
         idoutput2 = output['response']['categories']
         iddic = {}
@@ -306,7 +297,7 @@ try:
     cache_file2.close()
 except:
     CACHE_DICTION2 = {}
-    
+
 def make_request_using_cache(near_item,categoryId_item):
 #    baseurl = 'https://api.foursquare.com/v2/venues/search'
     paradic = {}
@@ -319,17 +310,15 @@ def make_request_using_cache(near_item,categoryId_item):
     paradic['v'] = '20191103'
     unique_ident = str(paradic['near'])+str(paradic['categoryId'])
     if unique_ident in CACHE_DICTION2:
-        #print("Getting cached data...")
         return CACHE_DICTION2[unique_ident]
     else:
-        #print("Making a request for new data...")
         CACHE_DICTION2[unique_ident] = search(near_item,categoryId_item)
         dumped_json_cache = json.dumps(CACHE_DICTION2)
         fw = open(CACHE_FNAME4,"w")
         fw.write(dumped_json_cache)
         fw.close() # Close the open file
         return CACHE_DICTION2[unique_ident]
-    
+
 def search(near_item,categoryId_item):
     baseurl = 'https://api.foursquare.com/v2/venues/search'
     paradic = {}
@@ -353,8 +342,6 @@ def getdata(near_item,categoryId_item):
         venuesdicitem['id'] = i['id']
         venuesdicitem['venue'] = i['name']
         venuesdicitem['address'] = i['location']['formattedAddress']
-        #venuesdicitem['photo id'] = i['id']
-        #venuesdicitem['photo link'] = i['categories'][0]['icon']['prefix']
         venuesdicitem['lat'] = i['location']['lat']
         venuesdicitem['lng'] = i['location']['lng']
         venueslist.append(venuesdicitem)
@@ -364,7 +351,6 @@ def getdata(near_item,categoryId_item):
 def db_list(orderby):
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
-#    limit =' ASC LIMIT 100'
     insertion = 'SELECT Wiki.University, Wiki.Motto, Wiki.Type, Wiki.Established, Wiki.Website FROM Wiki'
     if orderby.lower() == 'name':
         order =' ORDER BY Wiki.University '
@@ -379,7 +365,7 @@ def db_list(orderby):
     else:
         pass
     cur.close()
-    
+
 def db_score():
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
@@ -389,14 +375,13 @@ def db_score():
     item = cur.fetchall()
     return item
     cur.close()
-    
+
 def db_rank(orderby):
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
     insertion = 'SELECT * FROM Rankings WHERE Region="United States"'
     if orderby.lower() == '2019 rank':
-        #order =' ORDER BY "2019 Rank" '
-        #cur.execute(insertion+order)
+
         cur.execute(insertion)
         item = cur.fetchall()
         return item
@@ -408,7 +393,7 @@ def db_rank(orderby):
     else:
         pass
     cur.close()
-    
+
 def db_website(university):
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
@@ -418,7 +403,7 @@ def db_website(university):
     item = cur.fetchone()
     return item
     cur.close()
-    
+
 def db_map(university):
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
